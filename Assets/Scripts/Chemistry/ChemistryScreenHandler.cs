e26a8bb29a109aeb16dc9c4aa111f2dfc460d458
nataliestag <nc05stag@siena.edu>
2021-07-01 21:07:46 -0400

    chem play/pause script, aldol & claisen

---
+++ Assets/Scripts/Chemistry/ChemistryScreenHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ChemistryScreenHandler : MonoBehaviour
+{
+    private Animator mechanismAnimator;
+    private GameObject lastMenuScreen;
+    private float animSpeed;
+    public void SetMechanismAnimator(Animator mechanismAnimator)
+    {
+        this.mechanismAnimator = mechanismAnimator;
+    }
+    public void SetLastMenuScreen(GameObject lastMenuScreen)
+    {
+        this.lastMenuScreen = lastMenuScreen;
+    }
+    public void EnableLastMenuScreen()
+    {
+        lastMenuScreen.SetActive(true);
+    }
+    public void Play()
+    {
+        mechanismAnimator.speed = animSpeed;
+    }
+    public void Pause()
+    {
+        animSpeed = mechanismAnimator.speed;
+        mechanismAnimator.speed = 0;
+    }
+}
