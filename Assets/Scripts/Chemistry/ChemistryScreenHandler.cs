9a5c0f4727e510b8050e31d4537b1ed1df420e03
Justin Marotta <jamarotta13@gmail.com>
2021-07-28 20:51:41 -0400

    chem handler update

--- Assets/Scripts/Chemistry/ChemistryScreenHandler.cs
+++ Assets/Scripts/Chemistry/ChemistryScreenHandler.cs
@@ -4,24 +4,40 @@ using UnityEngine;
 public class ChemistryScreenHandler : MonoBehaviour
 {
     private Animator mechanismAnimator;
+    private GameObject lastMechanism;
     private GameObject lastMenuScreen;
     private float animSpeed;
+
     public void SetMechanismAnimator(Animator mechanismAnimator)
     {
         this.mechanismAnimator = mechanismAnimator;
     }
+
     public void SetLastMenuScreen(GameObject lastMenuScreen)
     {
         this.lastMenuScreen = lastMenuScreen;
     }
+
+    public void SetLastMechanism(GameObject lastMechanism)
+    {
+        this.lastMechanism = lastMechanism;
+    }
+
     public void EnableLastMenuScreen()
     {
         lastMenuScreen.SetActive(true);
     }
+
+    public void DisableLastMechanism()
+    {
+        lastMechanism.SetActive(false);
+    }
+
     public void Play()
     {
         mechanismAnimator.speed = animSpeed;
     }
+
     public void Pause()
     {
         animSpeed = mechanismAnimator.speed;
