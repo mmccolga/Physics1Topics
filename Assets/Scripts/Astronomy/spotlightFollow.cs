580a4aca5b5d255c71fc786ab994fccce4be00c1
Michele McColgan <mmccolgan@siena.edugit config --listgit config --global user.email mmccolgan@siena.edugit config --list>
2021-08-25 21:10:43 -0400

    added new torque animations and astronomy lunar phases with Ben

---
+++ Assets/Scripts/Astronomy/spotlightFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spotlightFollow : MonoBehaviour
+{
+    public GameObject theObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(theObject.transform);
+    }
+}
